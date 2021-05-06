7e2b42cc7eb05174ef4fd0579f6c330f241762a8
akselsgraveris <aksels.graveris@gmail.com>
2021-05-07 00:44:09 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [receiveNotifications, setReceiveNotifications] = React.useState(
+        false
+    );
+    const [receiveUpdates, setReceiveUpdates] = React.useState(false);
+
+    useEffect(() => {
+        // fetch("/api").then((response) =>
+        //     response.json().then((data) => {
+        //        ???
+        //     })
+        // );
+    }, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        let dataIn = JSON.parse(document.querySelector(".data-in").innerHTML);
+        dataIn["first_name"] = firstName;
+        dataIn["last_name"] = lastName;
+        dataIn["password"] = password;
+        dataIn["receive_notifications"] = receiveNotifications;
+        dataIn["receive_updates"] = receiveUpdates;
+        let dataOut = JSON.stringify(dataIn);
+
+        fetch("/api/onboarding/", {
+            method: "POST",
+            body: dataOut,
+        });
+    };
+
+    return (
+        <form className="onboarding__form flex--column" onSubmit={handleSubmit}>
+            <h1>Onboard</h1>
+            <label className="flex--column">
+                First Name:
+                <input
+                    name="firstName"
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                />
+            </label>
+
+            <label>
+                Last Name:
+                <input
+                    name="firstName"
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                />
+            </label>
+
+            <label>
+                Password:
+                <input
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+
+            <label className="check__box">
+                <input
+                    name="receiveUpdates"
+                    type="checkbox"
+                    onChange={(e) => setReceiveNotifications(e.target.value)}
+                    required
+                />
+                <p>Receive notifications on their project completions</p>
+            </label>
+
+            <label className="check__box">
+                <input
+                    name="receiveNotifications"
+                    type="checkbox"
+                    onChange={(e) => setReceiveUpdates(e.target.value)}
+                    required
+                />
+                <p>Receive latest updates about our services and offers.</p>
+            </label>
+
+            <button>Submit</button>
+        </form>
+    );
 }
 
 export default App;
